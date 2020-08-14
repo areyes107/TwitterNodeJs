6b4e6a6d2cbf917f043d3bc117a4842d139b712c
Angel Reyes <angelmrg2000@hotmail.com>
2020-08-13 18:59:53 -0600

    models/reply.model: schema wasn't added

--- src/models/reply.model.js
+++ src/models/reply.model.js
@@ -1,9 +1,13 @@
 const Mongoose = require('mongoose');
 const Schema = Mongoose.Schema;
 
-const ReplySchema = ({
+const ReplySchema = Schema ({
     author: {type: Schema.Types.ObjectId, ref: 'user'},
     content: String
-});
+},
+{
+    versionKey: false
+}
+);
 
 module.exports = Mongoose.model('reply', ReplySchema);
